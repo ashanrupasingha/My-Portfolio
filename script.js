18363bcb2f72836510b81a079d7ec8cc44a36052
Ashan <ashannuwantha050@gmail.com>
2024-10-20 09:05:40 +0530

    done

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".multiple-text",{
-    strings:["Frontend Developer","Backend Developer"],
+    strings:["Frontend Developer","Backend Developer",],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
